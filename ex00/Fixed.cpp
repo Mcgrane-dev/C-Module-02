03ebfa0384871a7b672f6e680c6e1d3b5b78c3f7
Jack Mcgrane <jmcgrane@f1r1s19.codam.nl>
2026-05-07 13:33:26 +0200

    x

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,21 +6,27 @@
 /*   By: jmcgrane <jmcgrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 14:45:40 by jmcgrane          #+#    #+#             */
-/*   Updated: 2026/05/07 12:18:26 by jmcgrane         ###   ########.fr       */
+/*   Updated: 2026/05/07 13:26:42 by jmcgrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+Fixed::Fixed(Fixed const &object)
+{
+	this->value = object.getRawBits();
+}
 
 void Fixed::setRawBits(int const raw)
 {
 	this->value = raw;
 }
 
-int Fixed::getRawBits(void)
+int Fixed::getRawBits(void) const
 {
 	return this->value;
 }
 
 Fixed::Fixed() : value(0) {}
+
 Fixed::~Fixed(){}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jmcgrane <jmcgrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 14:45:44 by jmcgrane          #+#    #+#             */
-/*   Updated: 2026/05/07 11:50:32 by jmcgrane         ###   ########.fr       */
+/*   Updated: 2026/05/07 13:26:24 by jmcgrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ private:
 public:
 	Fixed();
 	~Fixed();
+	Fixed(Fixed const &src);
 
-	int getRawBits(void);
-	void setRawBits(int const raw );
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
 
 };
 
